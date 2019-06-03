4ffefcf23954c2a544bbfab1dad7c177802337c6
Mel <zzidante@gmail.com>
2019-06-02 19:40:46 -0700

    Remove excess looping causing csv file rewrites, colorize output, rename vars

--- json_with_array_to_csv_generator.rb
+++ json_with_array_to_csv_generator.rb
@@ -8,9 +8,14 @@ command_line_input = ARGV || [];
 notify = Proc.new { |text| puts text }
 notify_and_exit = Proc.new { |text| notify.call(text); exit; }
 
+# make terminal completion/errors more apparent by coloring them & appending a special sign
+green_text = Proc.new { |text| "\e[32m--> #{text}\e[0m" }
+red_text = Proc.new { |text| "\e[31m!!! #{text}\e[0m" }
+
 # if its all empty, terminate
 command_line_input.empty? && notify_and_exit.call('[-j --json], [-c --csv] are valid flags. Use -h or --help for help with how to use them.')
 
+# handle arguments
 command_line_input.each_with_index do |args, index|
   case args
   when '--json', '-j'
@@ -46,49 +51,57 @@ json_info = File.open("#{json_filename}.json").read
 
 # transform to Ruby object for parsing
 begin
-  json_info_now_ruby_object = JSON.parse(json_info)
+  data = JSON.parse(json_info)
 rescue
-  notify_and_exit.call('JSON was invalid. Check format and try again.')
+  notify_and_exit.call(red_text.call('JSON was invalid. Check format and try again.'))
 end
 
-json_info_now_ruby_object.each do |k, array_entry|
+# cycle through each top level key to parse its array into a file
+data.each do |k, array_entry|
   headers_collection = []
   sorted_collection = {}
 
+  # get all unique headers across entries
   array_entry.each do |entry|
     entry.keys.each do |key|
       headers_collection.push(key) unless headers_collection.include?(key)
     end
   end
 
+  # create missing keys per entry in order to make all entries keys identical,
+  # default these new keys to a value of ''
   array_entry.each do |entry|
     headers_collection.each do |header|
-      # if the object doesn't have the header, default a value to ''
       entry[header] ||= ''
     end
   end
 
   # make sure hashes are sorted so that their values line up for later
-  # formulate new entry objects because mutating hashes is weird and oddly unreliable
-  sorted_collection[k] = []
-
-  sorted_collection[k] = array_entry.map.with_index do |entry, i|
+  # formulate new entry objects because mutating hashes is weird and unreliable
+  sorted_collection = array_entry.map.with_index do |entry, i|
     array_entry[i].sort_by { |ky, _| ky }.to_h
   end
 
-  # creates new CSVs for each top level key JIC they exist.
-  sorted_collection.each do |key, collection|
-    sorted_collection[key].each_with_index do |entry, i|
-      csv = CSV.open("#{csv_filename}-#{key}.csv", "wb") do |csv|
-        # we only need 1 example of the headers since all entries now have them.
-        csv << sorted_collection[key][0].keys
-        collection.each do |entry|
-          csv << entry.values
-        end
-      end
-    end
+  csvname = "#{csv_filename}-#{k}.csv"
 
-    notify.call("#{Dir.pwd}/#{csv_filename}-#{key}.csv has been generated.")
+  # ensure that the new files won't overwrite any existing ones
+  begin
+    all_csvs = Dir["**/*.csv"]
+    all_csvs.include?(csvname) && raise
+  rescue
+    notify_and_exit.call(red_text.call('Using this csv name will cause files to be overwritten. Please use a unique name and try again.'))
   end
-end
 
+  # create the CSV
+  CSV.open(csvname, "wb") do |csv|
+    # we only need 1 example of the headers since all entries now have them.
+    csv << sorted_collection[0].keys
+    # now insert the rows
+    sorted_collection.each do |entry|
+      csv << entry.values
+    end
+  end
+
+  notify.call(green_text.call("#{Dir.pwd}/#{csv_filename}-#{k}.csv has been generated."))
+end
+notify_and_exit.call(green_text.call('done'))
